5e071907b3271bf0dd0f6ac2658fe0c70b23f488
Javier Heisecke <javierheiseckee@gmail.com>
2024-08-15 11:11:35 -0400

    Adds a search bar and the main view with Tabs

---
+++ MoviesApp/MoviesApp/Presentation/MainView.swift
@@ -0,0 +1,48 @@
+//
+//  MainView.swift
+//  MoviesApp
+//
+//  Created by Javier Heisecke on 2024-08-15.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    // MARK: - Properties
+    
+    @State private var activeTab: Tab = .home
+    
+    var body: some View {
+        TabView(selection: $activeTab,
+                content:  {
+            HomeView(viewModel: .init())
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+                .tag(Tab.home)
+            Text("Search")
+                .tabItem {
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+                .tag(Tab.search)
+            Text("Watch list")
+                .tabItem {
+                    Label("Watch list", systemImage: "bookmark")
+                }
+                .tag(Tab.watchlist)
+        })
+    }
+    
+    // MARK: - Types
+    
+    enum Tab {
+        case home
+        case search
+        case watchlist
+    }
+}
+
+#Preview {
+    MainView()
+}
