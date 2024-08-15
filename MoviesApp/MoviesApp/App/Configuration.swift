73aaa046d69cb42bd362b91e010f181c5fbc753c
Javier Heisecke <javierheiseckee@gmail.com>
2024-08-15 09:56:10 -0400

    Defines project structure

---
+++ MoviesApp/MoviesApp/App/Configuration.swift
@@ -0,0 +1,28 @@
+//
+//  Configuration.swift
+//  MoviesApp
+//
+//  Created by Javier Heisecke on 2024-08-15.
+//
+
+import Foundation
+
+enum Configuration {
+    
+    private enum Keys {
+        static let tmdbApiKey = "TMDB_API_KEY"
+        static let tmdbAccessToken = "TMBD_API_READ_ACCESS_TOKEN"
+    }
+    
+    private static var infoDictionary: [String: Any] {
+        Bundle.main.infoDictionary ?? [:]
+    }
+    
+    static var tmdbApiKey: String {
+        infoDictionary[Keys.tmdbApiKey] as! String
+    }
+    
+    static var clearSkyAPIKey: String {
+        infoDictionary[Keys.tmdbAccessToken] as! String
+    }
+}
---
+++ MoviesApp/MoviesApp/App/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Javier Heisecke on 2024-08-15.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
