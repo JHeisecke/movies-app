8d1a5531ebf34629aa2fc19ef7801c099e504979
Javier Heisecke <javierheiseckee@gmail.com>
2024-08-17 01:55:07 -0400

    set debug case again

--- MoviesApp/MoviesApp/Data/Networking/APIClient.swift
+++ MoviesApp/MoviesApp/Data/Networking/APIClient.swift
@@ -22,19 +22,19 @@ protocol APIClientProtocol {
 final class APIClient: NSObject, APIClientProtocol {
     
     private func performRequest<T: Decodable>(endpoint: Endpoint, decoder: JSONDecoder) async throws -> T {
-//#if DEBUG
-//        if let mockfile = endpoint.mockFile, let path = Bundle.main.path(forResource: mockfile, ofType: "json") {
-//            let url = URL(fileURLWithPath: path)
-//            let data = try Data(contentsOf: url)
-//            do {
-//                let decodedData = try decoder.decode(T.self, from: data)
-//                return decodedData
-//            } catch {
-//                print(String(describing: error))
-//                throw error
-//            }
-//        }
-//#endif
+#if DEBUG
+        if let mockfile = endpoint.mockFile, let path = Bundle.main.path(forResource: mockfile, ofType: "json") {
+            let url = URL(fileURLWithPath: path)
+            let data = try Data(contentsOf: url)
+            do {
+                let decodedData = try decoder.decode(T.self, from: data)
+                return decodedData
+            } catch {
+                print(String(describing: error))
+                throw error
+            }
+        }
+#endif
         
         guard let urlRequest = try? endpoint.asRequest() else {
             throw APIError.invalidRequest
